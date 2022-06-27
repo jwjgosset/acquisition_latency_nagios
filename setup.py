8d0a6ab11968136258708a2c0e343494ac90cf38
Jonathan Gosset <jonathan.gosset@nrcan-rncan.gc.ca>
2022-06-27 13:40:42 +0000

    changed version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='eew_nagios',
-    version='1.0',
+    version='1.2',
     description=('Custom Nagios Plugins to check EEW Aquisionion servers ' +
                  'and Instruments'),
     author='Jonathan Gosset',
